8be893b43c599d2d93558f5697139fa011c68e0d
CodeRobin <119474459+YonggunJung@users.noreply.github.com>
2024-03-23 15:08:21 +0900

    여러가지 출력 방법 2024.3.23
    2024.3.23

---
+++ 덧셈식 출력하기.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programers
+{
+    internal class 덧셈식_출력하기
+    {
+        static void Main6(string[] args)
+        {
+            String[] s;
+
+            Console.Clear();
+            s = Console.ReadLine().Split(' ');
+
+            int a = Int32.Parse(s[0]);
+            int b = Int32.Parse(s[1]);
+
+            Console.WriteLine("{0} + {1} = {2}", a, b, a+b);
+        }
+
+        //다른 사람 풀이
+        //신기한 방법 많음, 결국 다 출력 방법임
+        //Console.WriteLine(""+a+" + "+b+" = {0}", a + b);
+        //Console.WriteLine($"{a} + {b} = {a+b}");
+        //Console.WriteLine(a + " + " + b + " = " + (a + b));
+    }
+}
