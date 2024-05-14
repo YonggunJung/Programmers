4f817715af10387e89f80f204fab342e2028c048
CodeRobin <119474459+YonggunJung@users.noreply.github.com>
2024-05-14 21:58:18 +0900

    2024.5.14

---
+++ 글자 지우기.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmers
+{
+    internal class 글자_지우기
+    {
+        //문제 설명
+        //문자열 my_string과 정수 배열 indices가 주어질 때,
+        //my_string에서 indices의 원소에 해당하는 인덱스의 글자를 지우고 이어 붙인 문자열을
+        //return 하는 solution 함수를 작성해 주세요.
+
+        //제한사항
+        //1 ≤ indices의 길이<my_string의 길이 ≤ 100
+        //my_string은 영소문자로만 이루어져 있습니다
+        //0 ≤ indices의 원소 < my_string의 길이
+        //indices의 원소는 모두 서로 다릅니다.
+
+        //입출력 예
+        //my_string                     indices                             result
+        //"apporoograpemmemprs"	        [1, 16, 6, 15, 0, 10, 11, 3]        "programmers"
+
+        //입출력 예 설명
+        //입출력 예 #1
+        //예제 1번의 my_string의 인덱스가 잘 보이도록 표를 만들면 다음과 같습니다.
+        //index         0	1	2	3	4	5	6	7	8	9	10	11	12	13	14	15	16	17	18
+        //my_string     a   p   p   o   r   o   o   g   r   a   p    e   m   m   e   m   p   r   s
+        //indices에 있는 인덱스의 글자들을 지우고 이어붙이면 "programmers"가 되므로 이를 return 합니다.
+        public string solution53(string my_string, int[] indices)
+        {
+            string answer = "";
+            List<int> li = new List<int>(indices);
+            li.Sort(new Comparison<int>((n1, n2) => n2.CompareTo(n1)));
+            foreach(int i in li)
+            {
+                my_string = my_string.Remove(i, 1);
+            }
+            answer = my_string;
+            return answer;
+        }
+        // 다른 사람 풀이
+        // 이런거 정말 편해 보이는 구만
+        //return new string (my_string.Where((w, index) => !indices.Contains(index)).ToArray());
+
+        // 다른사람풀이 2
+        //char[] arr = my_string.ToCharArray();
+        //string answer = "";
+        //for (int i = 0; i<indices.Length; i++)
+        //    arr[indices[i]] = ' ';
+        //answer = new string (arr).Replace(" ","");
+        //return answer;
+
+        static void Main53(string[] args)
+        {
+            int[] indices = { 1, 16, 6, 15, 0, 10, 11, 3 };
+            List<int> li = new List<int>(indices);
+            li.Sort(new Comparison<int>((n1, n2) => n2.CompareTo(n1)));
+
+            Console.WriteLine(li[0]);
+
+            string str = "apporoograpemmemprs";
+
+            foreach (int i in li)
+            {
+                str = str.Remove(i, 1);
+            }
+            Console.WriteLine(str);
+        }
+    }
+}
---
+++ 문자 개수 세기.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmers
+{
+    internal class 문자_개수_세기
+    {
+        //문제 설명
+        //알파벳 대소문자로만 이루어진 문자열 my_string이 주어질 때,
+        //my_string에서 'A'의 개수,
+        //my_string에서 'B'의 개수,...,
+        //my_string에서 'Z'의 개수,
+        //my_string에서 'a'의 개수,
+        //my_string에서 'b'의 개수,...,
+        //my_string에서 'z'의 개수를 순서대로 담은 길이 52의 정수 배열을 return 하는 solution 함수를 작성해 주세요.
+
+        //제한사항
+        //1 ≤ my_string의 길이 ≤ 1,000
+
+        //입출력 예
+        //my_string         result
+        //"Programmers"	    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 2, 0, 1, 0, 0, 3, 1, 0, 0, 0, 0, 0, 0, 0]
+
+        //입출력 예 설명
+        //입출력 예 #1
+
+        //예제 1번의 my_string에서 'P'가 1개,
+        //'a'가 1개,
+        //'e'가 1개,
+        //'g'가 1개,
+        //'m'이 2개,
+        //'o'가 1개,
+        //'r'가 3개,
+        //'s'가 1개 있으므로
+        //[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 2, 0, 1, 0, 0, 3, 1, 0, 0, 0, 0, 0, 0, 0]를 return 합니다.
+        public int[] solution51(string my_string)
+        {
+            int[] answer = new int[52];
+            List<int> li = new List<int>(answer);
+            string abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            for (int i = 0; i<my_string.Length; i++)
+            {
+                for (int j = 0; j < abc.Length; j++)
+                {
+                    if (my_string[i] == abc[j])
+                        li[j]++;
+                }
+            }
+            answer = li.ToArray();
+            return answer;
+        }
+        // 다른 사람 풀이
+        // Char.IsUpper() 이게 기억이 안났음
+        //    int[] answer = new int[52];
+        //    foreach(char c in my_string)
+        //    {
+        //        if(Char.IsUpper(c))
+        //            answer[c - 'A']++;
+        //        else
+        //            answer[26 + c - 'a']++;
+        //    }
+        //    return answer;
+        // A = 65, a = 97    
+    }
+}
