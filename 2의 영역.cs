ebc7bed545c0d4bb40a7ee5a24410bc6181d830f
CodeRobin <119474459+YonggunJung@users.noreply.github.com>
2024-05-15 16:02:29 +0900

    2024.5.15

---
+++ 2의 영역.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmers
+{
+    internal class _2의_영역
+    {
+        //문제 설명
+        //정수 배열 arr가 주어집니다.
+        //배열 안의 2가 모두 포함된 가장 작은 연속된 부분 배열을
+        //return 하는 solution 함수를 완성해 주세요.
+
+        //단, arr에 2가 없는 경우[-1] 을 return 합니다.
+
+        //제한사항
+        //1 ≤ arr의 길이 ≤ 100,000
+        //1 ≤ arr의 원소 ≤ 10
+
+        //입출력 예
+        //arr                           result
+        //[1, 2, 1, 4, 5, 2, 9]         [2, 1, 4, 5, 2]
+        //[1, 2, 1]                     [2]
+        //[1, 1, 1]                     [-1]
+        //[1, 2, 1, 2, 1, 10, 2, 1]     [2, 1, 2, 1, 10, 2]
+
+        //입출력 예 설명
+        //입출력 예 #1
+        //2가 있는 인덱스는 1번, 5번 인덱스뿐이므로
+        //1번부터 5번 인덱스까지의 부분 배열인 [2, 1, 4, 5, 2] 를 return 합니다.
+
+        //입출력 예 #2
+        //2가 한 개뿐이므로[2] 를 return 합니다.
+
+        //입출력 예 #3
+        //2가 배열에 없으므로[-1] 을 return 합니다.
+
+        //입출력 예 #4
+        //2가 있는 인덱스는 1번, 3번, 6번 인덱스이므로
+        //1번부터 6번 인덱스까지의 부분 배열인 [2, 1, 2, 1, 10, 2] 를 return 합니다.
+        public int[] solution59(int[] arr)
+        {
+            int[] answer = new int[] { };
+            List<int> li = new List<int>();
+            int f = Array.FindIndex(arr, n => n == 2);
+            int l = Array.FindLastIndex(arr, t => t == 2);
+            if (f == -1) li.Add(f);
+            else if (f == l)
+            {
+                li.Add(2);
+            }
+            else
+            {
+                for (int i = f; i <= l; i++)
+                {
+                    li.Add(arr[i]);
+                }
+
+            }
+            answer = li.ToArray();
+            return answer;
+        }
+        //다른사람 풀이
+        //내꺼 간소화 된 느낌
+        //int start = Array.IndexOf(arr, 2);
+        //if(start == -1) return new int[1]{ -1 };
+        //int end = Array.LastIndexOf(arr, 2);
+        //int[] answer = new int[end - start + 1];
+        //Array.Copy(arr, start, answer, 0, answer.Length);
+        //return answer;
+        static void Main59(string[] args)
+        {
+            int[] arr = { 1, 2, 1, 2, 1, 10, 2, 1 };
+            int[] ar = { 1, 1, 1, 10, 1 };
+            int f = Array.FindIndex(arr, n => n == 2);
+            int nt = Array.FindIndex(ar, n => n == 2);
+            int l = Array.FindLastIndex(arr, n => n == 2);
+
+            Console.WriteLine(f);
+            Console.WriteLine(l);
+            Console.WriteLine(nt);
+        }
+    }
+}
---
+++ 배열 조각하기.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmers
+{
+    internal class 배열_조각하기
+    {
+        //문제 설명
+        //정수 배열 arr와 query가 주어집니다.
+        //query를 순회하면서 다음 작업을 반복합니다.
+        //짝수 인덱스에서는 arr에서 query[i]번 인덱스를 제외하고
+        //배열의 query[i] 번 인덱스 뒷부분을 잘라서 버립니다.
+        //홀수 인덱스에서는 arr에서 query[i] 번 인덱스는 제외하고
+        //배열의 query[i] 번 인덱스 앞부분을 잘라서 버립니다.
+        //위 작업을 마친 후 남은 arr의 부분 배열을 return 하는 solution 함수를 완성해 주세요.
+
+        //제한사항
+        //5 ≤ arr의 길이 ≤ 100,000
+        //0 ≤ arr의 원소 ≤ 100
+        //1 ≤ query의 길이 < min(50, arr의 길이 / 2)
+        //query의 각 원소는 0보다 크거나 같고 남아있는 arr의 길이 보다 작습니다.
+
+        //입출력 예
+        //arr                       query           result
+        //[0, 1, 2, 3, 4, 5]        [4, 1, 2]       [1, 2, 3]
+
+        //입출력 예 설명
+        //입출력 예 #1
+        //이번에 매번 처리할 query의 값과 처리 전후의 arr의 상태를 표로 나타내면 다음과 같습니다.
+        //query의 값      query 처리 전          query 처리 후          비고
+        //4	              [0, 1, 2, 3, 4, 5]     [0, 1, 2, 3, 4]        0번 인덱스의 쿼리이므로 뒷부분을 자른다.
+        //1	              [0, 1, 2, 3, 4]        [1, 2, 3, 4]           1번 인덱스의 쿼리이므로 앞부분을 자른다.
+        //2	              [1, 2, 3, 4]           [1, 2, 3]              2번 인덱스의 쿼리이므로 뒷부분을 자른다.
+        //따라서[1, 2, 3] 을 return 합니다.
+        public int[] solution(int[] arr, int[] query)
+        {
+            // 문제 이해 잘못해서 오려걸리고,
+            //더하기 빼기 헷갈려서 오래걸림ㅠㅠ
+            int[] answer = new int[] { };
+            List<int> li = new List<int>(arr);
+            for (int i = 0; i<query.Length; i++)
+            {
+                if (i %2 == 0)
+                {
+                    li.RemoveRange(query[i] + 1, li.Count - query[i]-1);
+                }
+                else
+                {
+                    li.RemoveRange(0, query[i]);
+                }
+            }
+            answer = li.ToArray();
+            return answer;
+        }
+        //다른 사람 풀이
+        //Take()와 Skip()를 알아두면 편한거 같음.
+//        int[] answer = new int[] { };
+//        for(int i=0; i<query.Length; i++)
+//        {
+//            if (i % 2 == 0)
+//            {
+//                arr = arr.Take(query[i]+1).ToArray();
+//            }
+//            else
+//            {
+//                arr = arr.Skip(query[i]).ToArray();
+//            }
+//        }        
+//        answer = arr;
+//        return answer;
+static void Main60(string[] args)
+        {
+            int[] query = {4, 1, 2};
+            int[] arr = { 0, 1, 2, 3, 4, 5 };
+            List<int> li = new List<int>(arr);
+            //Console.WriteLine(li[5]);
+            li.RemoveRange(query[0] + 1, li.Count - query[0] + 1);
+            Console.WriteLine(li.Count);
+            li.RemoveRange(0, query[1]);
+            Console.WriteLine(li[0]);
+            li.RemoveRange(query[2] + 1, li.Count - query[2] + 1);
+            Console.WriteLine(li.Count);
+
+            //for (int i = 0;i<li.Count;i++)
+            //{
+            //    Console.WriteLine(li[i]);
+            //}
+        }
+    }
+}
