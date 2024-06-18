f2b1b80bfafc312f7b396d3fc47e11887cc4a885
CodeRobin <119474459+YonggunJung@users.noreply.github.com>
2024-06-18 23:39:59 +0900

    2024.6.18

---
+++ 뒤에서 5등 위로.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmers
+{
+    internal class 뒤에서_5등_위로
+    {
+        //문제 설명
+        //정수로 이루어진 리스트 num_list가 주어집니다.
+        //num_list에서 가장 작은 5개의 수를 제외한 수들을 오름차순으로
+        //담은 리스트를 return하도록 solution 함수를 완성해주세요.
+
+        //제한사항
+        //6 ≤ num_list의 길이 ≤ 30
+        //1 ≤ num_list의 원소 ≤ 100
+
+        //입출력 예
+        //num_list                                      result
+        //[12, 4, 15, 46, 38, 1, 14, 56, 32, 10]        [15, 32, 38, 46, 56]
+
+        //입출력 예 설명
+        //입출력 예 #1
+        //[12, 4, 15, 46, 38, 1, 14, 56, 32, 10]를 정렬하면
+        //[1, 4, 10, 12, 14, 15, 32, 38, 46, 56]이 되고,
+        //앞에서 부터 6번째 이후의 수들을 고르면[15, 32, 38, 46, 56]가 됩니다.
+        public int[] solution101(int[] num_list)
+        {
+            int[] answer = new int[] { };
+            List<int> li = new List<int>();
+            List<int> li2 = new List<int>(num_list);
+            li2 = li2.OrderBy(x => x).ToList();
+            for (int i = 5;i <li2.Count; i++)
+            {
+                li.Add(li2[i]);
+            }
+            answer = li.ToArray();
+            return answer;
+        }
+        //다른 사람  풀이
+        // skip()을 까먹었네 저번에 봤는데 까비
+        //return num_list.OrderBy(o => o).Skip(5).ToArray();
+    }
+}
