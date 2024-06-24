24c92a95115e12596da9c5faf0ae1d5cfaf3a28f
CodeRobin <119474459+YonggunJung@users.noreply.github.com>
2024-06-24 22:35:48 +0900

    2024.6.24

---
+++ 그림 확대.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmers
+{
+    internal class 그림_확대
+    {
+        //문제 설명
+        //직사각형 형태의 그림 파일이 있고,
+        //이 그림 파일은 1 × 1 크기의 정사각형 크기의 픽셀로 이루어져 있습니다.
+        //이 그림 파일을 나타낸 문자열 배열 picture과 정수 k가 매개변수로 주어질 때,
+        //이 그림 파일을 가로 세로로 k배 늘린 그림 파일을 나타내도록 문자열 배열을
+        //return 하는 solution 함수를 작성해 주세요.
+
+        //제한사항
+        //1 ≤ picture의 길이 ≤ 20
+        //1 ≤ picture의 원소의 길이 ≤ 20
+        //모든 picture의 원소의 길이는 같습니다.
+        //picture의 원소는 '.'과 'x'로 이루어져 있습니다.
+        //1 ≤ k ≤ 10
+
+        //입출력 예
+        //picture                                                                                       k       result
+        //[".xx...xx.", "x..x.x..x", "x...x...x", ".x.....x.", "..x...x..", "...x.x...", "....x...."]   2	    ["..xxxx......xxxx..", "..xxxx......xxxx..", "xx....xx..xx....xx", "xx....xx..xx....xx", "xx......xx......xx", "xx......xx......xx", "..xx..........xx..", "..xx..........xx..", "....xx......xx....", "....xx......xx....", "......xx..xx......", "......xx..xx......", "........xx........", "........xx........"]
+        //["x.x", ".x.", "x.x"]                                                                         3	    ["xxx...xxx", "xxx...xxx", "xxx...xxx", "...xxx...", "...xxx...", "...xxx...", "xxx...xxx", "xxx...xxx", "xxx...xxx"]
+        //입출력 예 설명
+        //입출력 예 #1
+
+        //예제 1번의 picture는 다음과 같습니다.
+
+        //.xx...xx.
+        //x..x.x..x
+        //x...x...x
+        //.x.....x.
+        //..x...x..
+        //...x.x...
+        //....x....
+        //이를 가로 세로로 k배, 즉 2배 확대하면 그림 파일은 다음과 같습니다.
+
+        //..xxxx......xxxx..
+        //..xxxx......xxxx..
+        //xx....xx..xx....xx
+        //xx....xx..xx....xx
+        //xx......xx......xx
+        //xx......xx......xx
+        //..xx..........xx..
+        //..xx..........xx..
+        //....xx......xx....
+        //....xx......xx....
+        //......xx..xx......
+        //......xx..xx......
+        //........xx........
+        //........xx........
+        //따라서["..xxxx......xxxx..",
+        //       "..xxxx......xxxx..",
+        //       "xx....xx..xx....xx",
+        //       "xx....xx..xx....xx",
+        //       "xx......xx......xx",
+        //       "xx......xx......xx",
+        //       "..xx..........xx..",
+        //       "..xx..........xx..",
+        //       "....xx......xx....",
+        //       "....xx......xx....",
+        //       "......xx..xx......",
+        //       "......xx..xx......",
+        //       "........xx........",
+        //       "........xx........"] 를 return 합니다.
+
+        //입출력 예 #2
+        //예제 2번의 picture는 다음과 같습니다.
+        //x.x
+        //.x.
+        //x.x
+        //이를 가로 세로로 k배, 즉 3배 확대하면 그림 파일은 다음과 같습니다.
+        //xxx...xxx
+        //xxx...xxx
+        //xxx...xxx
+        //...xxx...
+        //...xxx...
+        //...xxx...
+        //xxx...xxx
+        //xxx...xxx
+        //xxx...xxx
+        //따라서 ["xxx...xxx",
+        //        "xxx...xxx",
+        //        "xxx...xxx",
+        //        "...xxx...",
+        //        "...xxx...",
+        //        "...xxx...",
+        //        "xxx...xxx",
+        //        "xxx...xxx",
+        //        "xxx...xxx"] 를 return 합니다.
+        public string[] solution117(string[] picture, int k)
+        {
+            string[] answer = new string[] { };
+            List<string> li = new List<string>();
+
+            for (int i = 0; i < picture.Length; i++)
+            {
+                string p = "";
+                for (int j = 0; j < picture[0].Length; j++)
+                {
+                    for (int m = 0; m < k; m++)
+                    {
+                        p += picture[i][j];
+                    }
+                    Console.WriteLine(p);
+                }
+                for (int j = 0; j < k; j++)
+                {
+                    li.Add(p);
+                }
+            }
+            answer = li.ToArray();
+            return answer;
+        }
+        //다른 사람 풀이
+        //List<string> answer = new List<string>();
+        //string replace_a = "";
+        //string replace_b = "";
+        //for (int i = 0; i<k; i++)
+        //{
+        //    replace_a += ".";
+        //    replace_b += "x";
+        //}
+        //for (int i = 0; i<picture.Length; i++)
+        //{
+        //    picture[i] = picture[i].Replace(".", replace_a);
+        //     picture[i] = picture[i].Replace("x", replace_b);
+        //    for (int j =0; j<k; j++)
+        //    {
+        //        answer.Add(picture[i]);
+        //    }
+        //}
+        //return answer.ToArray();
+
+static void Main117(string[] args)
+        {
+            string[] picture = { "x.x", ".x.", "x.x" };
+            int k = 3;
+            List<string> li = new List<string>();
+
+            for (int i = 0; i < picture.Length; i++)
+            {
+                string p = "";
+                for (int j = 0; j < picture[0].Length; j++)
+                {
+                    for (int m = 0; m < k; m++)
+                    {
+                        p += picture[i][j];
+                    }
+                    Console.WriteLine(p);
+                }
+                for (int j = 0; j < k; j++)
+                {
+                    li.Add(p);
+                }
+                
+            }
+            answer = li.ToArray();
+                Console.WriteLine(picture.Length);
+        }
+    }
+}
---
+++ 날짜 비교하기.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmers
+{
+    internal class 날짜_비교하기
+    {
+        //문제 설명
+        //정수 배열 date1과 date2가 주어집니다.
+        //두 배열은 각각 날짜를 나타내며[year, month, day] 꼴로 주어집니다.
+        //각 배열에서 year는 연도를, month는 월을, day는 날짜를 나타냅니다.
+        //만약 date1이 date2보다 앞서는 날짜라면 1을,
+        //아니면 0을 return 하는 solution 함수를 완성해 주세요.
+
+        //제한사항
+        //date1의 길이 = date2의 길이 = 3
+        //0 ≤ year ≤ 10,000
+        //1 ≤ month ≤ 12
+        //day는 month에 따라 가능한 날짜로 주어집니다.
+
+        //입출력 예
+        //date1             date2               result
+        //[2021, 12, 28]    [2021, 12, 29]      1
+        //[1024, 10, 24]    [1024, 10, 24]      0
+
+        //입출력 예 설명
+        //입출력 예 #1
+        //date1이 date2보다 하루 앞서기 때문에 1을 return 합니다.
+
+        //입출력 예 #2
+        //date1과 date2는 날짜가 서로 같으므로 date1이 더 앞서는 날짜가 아닙니다.
+        //따라서 0을 return 합니다.
+        public int solution115(int[] date1, int[] date2)
+        {
+            int answer = 0;
+
+            if (date1[0] < date2[0]) answer = 1;
+            else if (date1[0] == date2[0] && date1[1] < date2[1]) answer = 1;
+            else if (date1[0] == date2[0] && date1[1] == date2[1] && date1[2] < date2[2]) answer = 1;
+            
+            return answer;
+        }
+
+        //다른 사람 풀이
+        //int d1 = int.Parse($"{date1[0]}{date1[1]}{date1[2]}");
+        //int d2 = int.Parse($"{date2[0]}{date2[1]}{date2[2]}");
+        //return d1<d2? 1 : 0;
+
+        //DateTime d1 = new DateTime(date1[0], date1[1], date1[2]);
+        //DateTime d2 = new DateTime(date2[0], date2[1], date2[2]);
+        //return d1<d2? 1 : 0;
+    }
+}
