e1a9e124b822d23e7ec73d1c139e0f7da1454491
CodeRobin <119474459+YonggunJung@users.noreply.github.com>
2024-07-21 14:01:16 +0900

    2024.7.21

---
+++ 모음 제거.cs
@@ -0,0 +1,46 @@
+﻿using Programmers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmers
+{
+    internal class 모음_제거
+    {
+        //문제 설명
+        //영어에선 a, e, i, o, u 다섯 가지 알파벳을 모음으로 분류합니다.
+        //문자열 my_string이 매개변수로 주어질 때 모음을 제거한 문자열을
+        //return하도록 solution 함수를 완성해주세요.
+
+        //제한사항
+        //my_string은 소문자와 공백으로 이루어져 있습니다.
+        //1 ≤ my_string의 길이 ≤ 1,000
+
+        //입출력 예
+        //my_string                 result
+        //"bus"	                    "bs"
+        //"nice to meet you"	    "nc t mt y"
+
+        //입출력 예 설명
+        //입출력 예 #1
+        //"bus"에서 모음 u를 제거한 "bs"를 return합니다.
+
+        //입출력 예 #1
+        //"nice to meet you"에서 모음 i, o, e, u를 모두 제거한 "nc t mt y"를 return합니다.
+        public string solution178(string my_string)
+        {
+            string answer = "";
+            foreach(char c in my_string)
+            {
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                    continue;
+                answer += c;
+            }
+            return answer;
+        }
+        // 다른 사람 풀이
+        //return Regex.Replace(my_string,"[a,e,i,o,u]","");
+    }
+}
---
+++ 최댓값 만들기 1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmers
+{
+    internal class 최댓값_만들기_1
+    {
+        //문제 설명
+        //정수 배열 numbers가 매개변수로 주어집니다.
+        //numbers의 원소 중 두 개를 곱해 만들 수 있는 최댓값을
+        //return하도록 solution 함수를 완성해주세요.
+
+        //제한사항
+        //0 ≤ numbers의 원소 ≤ 10,000
+        //2 ≤ numbers의 길이 ≤ 100
+
+        //입출력 예
+        //numbers                   result
+        //[1, 2, 3, 4, 5]               20
+        //[0, 31, 24, 10, 1, 9]        744
+
+        //입출력 예 설명
+        //입출력 예 #1
+        //두 수의 곱중 최댓값은 4 * 5 = 20 입니다.
+
+        //입출력 예 #1
+        //두 수의 곱중 최댓값은 31 * 24 = 744 입니다.
+        public int solution180(int[] numbers)
+        {
+            int answer = 0;
+            Array.Sort(numbers);
+            answer = numbers[numbers.Length-1] * numbers[numbers.Length - 2];
+            return answer;
+        }
+        // 다른 사람 풀이
+        //numbers = numbers.OrderByDescending(x => x).ToArray();
+        //int answer = numbers[0] * numbers[1];
+    }
+}
