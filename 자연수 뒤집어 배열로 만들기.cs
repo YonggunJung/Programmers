00e70e13f654e5cdbaebef79ebb75803146a2971
CodeRobin <119474459+YonggunJung@users.noreply.github.com>
2024-09-08 14:39:51 +0900

    2024.9.8

---
+++ 자연수 뒤집어 배열로 만들기.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersLv1
+{
+    internal class 자연수_뒤집어_배열로_만들기
+    {
+        //문제 설명
+        //자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요.
+        //예를들어 n이 12345이면[5, 4, 3, 2, 1] 을 리턴합니다.
+
+        //제한 조건
+        //n은 10,000,000,000이하인 자연수입니다.
+
+        //입출력 예
+        //n	                return
+        //12345	            [5, 4, 3, 2, 1]
+        public long[] solution8(long n)
+        {
+            long[] answer = new long[] { };
+            List<long> li = new List<long>();
+            while (n > 0)
+            {
+                long i = n % 10;
+                n /= 10;
+                li.Add(i);
+            }
+            answer = li.ToArray();
+            return answer;
+        }
+        //데이터 타입을 맞춰줘야 한다.
+    }
+}
---
+++ 정수 내림차순으로 배치하기.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersLv1
+{
+    internal class 정수_내림차순으로_배치하기
+    {
+        //문제 설명
+        //함수 solution은 정수 n을 매개변수로 입력받습니다.
+        //n의 각 자릿수를 큰것부터 작은 순으로 정렬한 새로운 정수를 리턴해주세요.
+        //예를들어 n이 118372면 873211을 리턴하면 됩니다.
+
+        //제한 조건
+        //n은 1이상 8000000000 이하인 자연수입니다.
+
+        //입출력 예
+        //n             return
+        //118372	    873211
+        public long solution9(long n)
+        {
+            //별거 아닌건데 헷갈리네 ㅠㅠ
+            long answer = 0;
+            string str = new string(n.ToString().OrderByDescending(c => c).ToArray());
+            answer = long.Parse(str);
+            return answer;
+        }
+    }
+}
