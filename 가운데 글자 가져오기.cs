68edd1b8c62d8f5c603699175f8b5cacc3ffd595
CodeRobin <119474459+YonggunJung@users.noreply.github.com>
2024-09-16 18:03:00 +0900

    2024.9.16

---
+++ 가운데 글자 가져오기.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersLv1
+{
+    internal class 가운데_글자_가져오기
+    {
+        //문제 설명
+        //단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요.
+        //단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+
+        //재한사항
+        //s는 길이가 1 이상, 100이하인 스트링입니다.
+
+        //입출력 예
+        //s                     return
+        //"abcde"	            "c"
+        //"qwer"	            "we"
+        public string solution21(string s)
+        {
+            string answer = "";
+            if (s.Length % 2 == 1)
+                answer += s[s.Length/2];
+            else
+                answer = s.Substring(s.Length/2-1,2);
+            return answer;
+        }
+        
+        //다른사람 풀이
+        // 깔끔하네
+        //int n = (s.Length + 1) % 2;
+        //int l = s.Length / 2 - n;
+        //return s.Substring(l, n + 1);
+    }
+}
---
+++ 문자열 내림차순으로 배치하기.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersLv1
+{
+    internal class 문자열_내림차순으로_배치하기
+    {
+        //문제 설명
+        //문자열 s에 나타나는 문자를 큰것부터 작은 순으로 정렬해 새로운 문자열을 리턴하는 함수,
+        //solution을 완성해주세요.
+        //s는 영문 대소문자로만 구성되어 있으며,
+        //대문자는 소문자보다 작은 것으로 간주합니다.
+
+        //제한 사항
+        //str은 길이 1 이상인 문자열입니다.
+
+        //입출력 예
+        //s	                return
+        //"Zbcdefg"	        "gfedcbZ"
+        public string solution24(string s)
+        {
+            string answer = "";
+            char[] chars = s.ToCharArray();
+            Array.Sort(chars, (x, y) => y.CompareTo(x));
+            answer = new string(chars);
+            return answer;
+        }
+
+        //다른사람 풀이
+        // 이건 생각지도 못했네
+        //string answer = string.Concat(s.OrderByDescending(m => m));
+    }
+}
