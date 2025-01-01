6e7ea09a994ec547a3a22c5825ee6b259946e81f
CodeRobin <119474459+YonggunJung@users.noreply.github.com>
2025-01-01 17:29:07 +0900

    2025.1.1

---
+++ PCCE 기출문제 1번 문자 출력001.cpp
@@ -0,0 +1,22 @@
+
+//문제 설명
+//주어진 코드는 변수에 데이터를 저장하고 출력하는 코드입니다.
+//아래와 같이 출력되도록 빈칸을 채워 코드를 완성해 주세요.
+//
+//출력 예시
+//3
+//2
+//1
+//Let's go!
+#include <iostream>
+
+using namespace std;
+
+int main001(void) {
+    string message = "Let's go!";
+
+        cout << "3\n2\n1" << endl;
+        cout << message << endl;
+
+    return 0;
+}
