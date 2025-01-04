929a5a5d451b6eb702d5f144b74d6141b37ad891
CodeRobin <119474459+YonggunJung@users.noreply.github.com>
2025-01-04 16:46:02 +0900

    2025.1.4

---
+++ PCCE 기출문제 1번 출력009.cpp
@@ -0,0 +1,25 @@
+
+// 문제 설명
+//주어진 초기 코드는 변수에 데이터를 저장하고 출력하는 코드입니다.아래와 같이 출력되도록 빈칸을 채워 코드를 완성해 주세요.
+
+//출력 예시
+//Spring is beginning
+//13
+//310
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main009(void) {
+    string msg = "Spring is beginning";
+    int val1 = 3;
+    string val2 = "3";
+
+    cout << msg << endl;
+    cout << val1 + 10 << endl;
+    cout << val2 + "10" << endl;
+
+    return 0;
+}
